5e822f0bfd826bcf566d5ca60b8cf2379624e309
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-10 09:24:49 +0800

    - Error Fixed

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), 
-    path('posts/', views.post_list, name='post_list'),  
+    path('post/', views.post_list, name='post_list'), 
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_create, name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 from .models import Post
 from django.http import HttpResponse
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+         form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def home(request):
     return HttpResponse("Welcome to the Blog!")
