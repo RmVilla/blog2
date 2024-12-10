e5da12013910e6918186ce55026867190c080002
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-10 09:53:30 +0800

    - Image post handling fixed

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='blog/user_profile/', blank=True, null=True)
+    picture = models.ImageField(upload_to='post_image/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -107,6 +107,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
