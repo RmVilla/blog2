3619581a5333ae187900fc6760e0c25eac15c4fa
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-12 08:36:49 +0800

    Some Debugging

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Post
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
 
 class PostForm(forms.ModelForm):
     class Meta:
